2db3fa3675a98a1ec0438cbf515a2300833e390f
marwan-trade <trademarwan77@gmail.com>
2025-05-15 17:36:44 +0300

    version 0.1.2

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'  # This is the namespace for the app, used in templates and URL reversing
 urlpatterns = [
     path('', views.home, name='home'),
+    path("base/", views.base, name="base"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("search/", views.search, name="search"), # Assuming you have a search view
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Article, Category
+from django.http import HttpResponse
 
 # Create your views here.
+
+def base(request):
+    return render(request, 'blog/base.html')
 
 
 def home(request):
     articles = Article.objects.filter(published=True).order_by('-created_at')
     return render(request, 'blog/home.html', {'articles': articles})
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
+
+def search(request):
+    return HttpResponse("This is the search page.")
